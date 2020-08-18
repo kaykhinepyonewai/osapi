ecb5f9e4fa688c76ec074a404aee6ce65d8749e2
kaykhinepyonewai <kaykhinepyonewai@gmail.com>
2020-08-18 15:54:37 +0630

    Api Creating, Item Resource

---
+++ app/Order.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+     protected $fillable = [
+        'voucherno', 'orderdate', 'status', 'note', 'total', 'user_id'
+    ];
+
+
+
+      public function items($value='')     //appear object ->belongsTo
+    {
+    	return $this->belongsToMany('\App\Item','order_detail')
+    				->withPivot('qty')
+    				->withTimestamps();
+
+
+    }
+
+    public function user($value='')
+    {
+    	return $this->belongsTo('App\User');
+    }
+}
